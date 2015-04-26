f86b5ab6b277598407811b1ebf617107e032861c
Julien Lamy <julien@seasofcheese.net>
2015-04-26 18:04:28 +0200

    Add Media class.

---
+++ src/redmill/media.py
@@ -0,0 +1,45 @@
+# This file is part of Redmill.
+#
+# Redmill is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Redmill is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Redmill.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+import sqlalchemy
+import sqlalchemy.orm
+
+import redmill.database
+
+class Media(redmill.database.Base):
+    __tablename__ = "media"
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    title = sqlalchemy.Column(sqlalchemy.Unicode, nullable=False)
+    author = sqlalchemy.Column(sqlalchemy.Unicode, nullable=False)
+    keywords = sqlalchemy.Column(sqlalchemy.Unicode, nullable=False)
+    filename = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    album_id = sqlalchemy.Column(
+        sqlalchemy.Integer, sqlalchemy.ForeignKey('album.id'))
+
+    album = sqlalchemy.orm.relationship(
+        "Album", backref=sqlalchemy.orm.backref("media"))
+
+    def __init__(self, *args, **kwargs):
+        redmill.database.Base.__init__(self, *args, **kwargs)
+        if not self.filename:
+            self.filename = redmill.database.get_filesystem_path(self.title)
+
+    def _get_location(self):
+        return os.path.join(self.album.path, self.filename)
+
+    location = property(_get_location)
---
+++ tests/test_media.py
@@ -0,0 +1,49 @@
+# encoding: utf-8
+
+import unittest
+
+import sqlalchemy
+import sqlalchemy.orm
+
+import redmill.database
+
+class TestMedia(unittest.TestCase):
+
+    def setUp(self):
+        self.engine = sqlalchemy.create_engine("sqlite:///:memory:", echo=False)
+        redmill.database.Base.metadata.create_all(self.engine)
+
+        redmill.database.Session = sqlalchemy.orm.sessionmaker(bind=self.engine)
+        redmill.database.session = redmill.database.Session()
+
+        self.album = redmill.Album(name=u"Röôt album")
+        redmill.database.session.add(self.album)
+        redmill.database.session.commit()
+
+    def tearDown(self):
+        pass
+
+    def test_constructor(self):
+        media = redmill.Media(
+            title=u"Mÿ îmage", author=u"John Doe",
+            keywords=u", ".join(["foo", "bar"]), album_id=self.album.id)
+
+        self.assertEqual(media.filename, "My_image")
+
+        redmill.database.session.add(media)
+        redmill.database.session.commit()
+
+        self.assertEqual(media.album, self.album)
+        self.assertEqual(len(media.album.media), 1)
+        self.assertEqual(media.album.media[0], media)
+
+        media_collection = [x for x in redmill.database.session.query(redmill.Media)]
+        self.assertEqual(len(media_collection), 1)
+        self.assertEqual(media_collection[0].title, u"Mÿ îmage")
+        self.assertEqual(media_collection[0].author, "John Doe")
+        self.assertEqual(media_collection[0].keywords.split(", "), ["foo", "bar"])
+
+        self.assertEqual(media_collection[0].location, "Root_album/My_image")
+
+if __name__ == '__main__':
+    unittest.main()
