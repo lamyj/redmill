68819c6d8ade46b1377ca4e271630c758f49d766
Julien Lamy <julien@seasofcheese.net>
2015-05-21 21:01:15 +0200

    Fix main __init__

--- src/redmill/__init__.py
+++ src/redmill/__init__.py
@@ -13,15 +13,11 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Redmill.  If not, see <http://www.gnu.org/licenses/>.
 
-import libxmp
-
-xml_namespace = "https://github.com/lamyj/redmill"
-xml_prefix = "rm"
-libxmp.XMPMeta.register_namespace(xml_namespace, xml_prefix)
-
-from derivative import Derivative
-from image import Image
 import processor
-from catalog import Catalog
-from catalog_creator import CatalogCreator
-from exporter import Exporter
+
+import database
+import magic
+
+import models
+import views
+import controller
