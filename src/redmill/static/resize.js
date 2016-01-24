5619918c2e906cc3aa0a11edad40c8959dcbd70a
Julien Lamy <julien@seasofcheese.net>
2016-01-24 20:14:24 +0100

    Store ratio in crop parameters

--- src/redmill/static/resize.js
+++ src/redmill/static/resize.js
@@ -13,8 +13,8 @@ resize.init = function(derivative_url, media_url, operations) {
             operations.forEach(function(operation) {
                 if(operation[0] === 'crop') {
                     resize.setSelection({
-                        left: operation[1][0], top: operation[1][1],
-                        width: operation[1][2], height: operation[1][3]});
+                        left: operation[1].left, top: operation[1].top,
+                        width: operation[1].width, height: operation[1].height});
                 }
             });
 
@@ -24,21 +24,25 @@ resize.init = function(derivative_url, media_url, operations) {
 
     document.querySelector('#selection').drag = {
         action: null, position: null, corner: null };
-    document.querySelector('#selection>rect').addEventListener(
+    document.querySelector('#selection__frame').addEventListener(
         'mousedown', resize.onMoveStart);
 
-    [].forEach.call(
-        document.querySelectorAll('#selection>circle'),
+    [
+        document.querySelector('#selection__nw-handle'),
+        document.querySelector('#selection__se-handle'),
+    ].forEach(
         function(x) { x.addEventListener('mousedown', resize.onResizeStart); }
     );
 
-    document.querySelector('#selection_ratio #free').addEventListener(
+    document.querySelector('#selection-ratio__free').addEventListener(
         'click', resize.setRatioType.bind(undefined, 'free'));
-    document.querySelector('#selection_ratio #fixed').addEventListener(
+    document.querySelector('#selection-ratio__fixed').addEventListener(
         'click', resize.setRatioType.bind(undefined, 'fixed'));
-}
+};
 
 resize.getSelection = function() {
+    var selection = document.querySelector('#selection');
+
     var transform = selection.getAttribute('transform');
     if(transform === null) {
         throw new Error('No selection');
@@ -49,12 +53,12 @@ resize.getSelection = function() {
     var left = matrix[4];
     var top = matrix[5];
 
-    var rect = selection.querySelector('rect');
-    var width = parseFloat(rect.getAttribute('width'));
-    var height = parseFloat(rect.getAttribute('height'));
+    var frame = selection.querySelector('#selection__frame');
+    var width = parseFloat(frame.getAttribute('width'));
+    var height = parseFloat(frame.getAttribute('height'));
 
     return {left: left, top: top, width: width, height: height};
-}
+};
 
 resize.setSelection = function(area) {
     var canvas = document.querySelector('#canvas');
@@ -68,12 +72,16 @@ resize.setSelection = function(area) {
     area.width = Math.max(0, area.width);
     area.height = Math.max(0, area.height);
 
-    var ratio_width = document.querySelector('#selection_ratio #width');
-    var ratio_height = document.querySelector('#selection_ratio #height');
-    if(resize.getRatioType() === 'fixed' &&
-        ratio_width.validity.valid && ratio_height.validity.valid) {
-        var ratio =
-            parseFloat(ratio_width.value) / parseFloat(ratio_height.value);
+    var ratio = null;
+    try {
+        ratio = resize.getRatio();
+    }
+    catch(e) {
+        // Ignore the error.
+    }
+
+    if(ratio !== null) {
+        ratio = ratio[0] / ratio[1];
         area.height = Math.round(area.width / ratio);
     }
 
@@ -81,18 +89,18 @@ resize.setSelection = function(area) {
     selection.setAttribute(
         'transform', 'matrix(1 0 0 1 '+area.left+' '+area.top+')');
 
-    var rect = selection.querySelector('rect');
-    rect.setAttribute('width', area.width);
-    rect.setAttribute('height', area.height);
+    var frame = selection.querySelector('#selection__frame');
+    frame.setAttribute('width', area.width);
+    frame.setAttribute('height', area.height);
 
-    var se_handle = selection.querySelector('#se');
+    var se_handle = selection.querySelector('#selection__se-handle');
     se_handle.setAttribute('cx', area.width);
     se_handle.setAttribute('cy', area.height);
-}
+};
 
 resize.getRatioType = function() {
-    var radio_free = document.querySelector('#selection_ratio #free');
-    var radio_fixed = document.querySelector('#selection_ratio #fixed');
+    var radio_free = document.querySelector('#selection-ratio__free');
+    var radio_fixed = document.querySelector('#selection-ratio__fixed');
 
     if(radio_free.checked) {
         return 'free';
@@ -103,13 +111,13 @@ resize.getRatioType = function() {
     else {
         throw new Error('Invalid ratio type');
     }
-}
+};
 
 resize.setRatioType = function(type) {
-    var radio_free = document.querySelector('#selection_ratio #free');
-    var radio_fixed = document.querySelector('#selection_ratio #fixed');
-    var width_control = document.querySelector('#selection_ratio #width');
-    var height_control = document.querySelector('#selection_ratio #height');
+    var radio_free = document.querySelector('#selection-ratio__free');
+    var radio_fixed = document.querySelector('#selection-ratio__fixed');
+    var width_control = document.querySelector('#selection-ratio__width');
+    var height_control = document.querySelector('#selection-ratio__height');
     if(type === 'free') {
         radio_free.checked = true;
         radio_fixed.checked = false;
@@ -137,7 +145,29 @@ resize.setRatioType = function(type) {
     else {
         throw new Error('Unknown ratio type: '+type);
     }
-}
+};
+
+resize.getRatio = function() {
+    var ratio = undefined;
+
+    var type = resize.getRatioType();
+    if(type === 'free') {
+        ratio = null;
+    }
+    else if(type === 'fixed') {
+        var width = document.querySelector('#selection-ratio__width');
+        var height = document.querySelector('#selection-ratio__height');
+        if(!width.validity.valid || !height.validity.valid) {
+            throw new Error('Invalid ratio');
+        }
+        ratio = [parseFloat(width.value), parseFloat(height.value)];
+    }
+    else {
+        throw new Error('Invalid ratio type: '+type);
+    }
+
+    return ratio;
+};
 
 resize.onMoveStart = function(event) {
     // Make sure the browser does not select the element when clicking it.
@@ -184,7 +214,7 @@ resize.onMoveStop = function(event) {
     canvas.removeEventListener('mouseup', resize.onMoveStop);
 
     resize.notifyChange();
-}
+};
 
 resize.onResizeStart = function(event) {
     // Make sure the browser does not select the element when clicking it.
@@ -198,7 +228,7 @@ resize.onResizeStart = function(event) {
     var canvas = document.querySelector('#canvas');
     canvas.addEventListener('mousemove', resize.onResize);
     canvas.addEventListener('mouseup', resize.onResizeStop);
-}
+};
 
 resize.onResize = function(event) {
     var selection = document.querySelector('#selection');
@@ -216,14 +246,14 @@ resize.onResize = function(event) {
 
     var area = resize.getSelection();
 
-    if(selection.drag.corner === 'nw') {
+    if(selection.drag.corner === 'selection__nw-handle') {
         area.left += delta[0];
         area.top += delta[1];
 
         area.width -= delta[0];
         area.height -= delta[1];
     }
-    else if(selection.drag.corner === 'se') {
+    else if(selection.drag.corner === 'selection__se-handle') {
         area.width += delta[0];
         area.height += delta[1];
     }
@@ -234,7 +264,7 @@ resize.onResize = function(event) {
     resize.setSelection(area);
 
     selection.drag.position = position;
-}
+};
 
 resize.onResizeStop = function(event) {
     var selection = document.querySelector('#selection');
@@ -246,15 +276,15 @@ resize.onResizeStop = function(event) {
     canvas.removeEventListener('mouseup', resize.onResizeStop);
 
     resize.notifyChange();
-}
+};
 
 resize.notifyChange = function() {
     var url = document.querySelector('#canvas').getAttribute('data-derivative');
 
-    var selection = resize.getSelection();
-    var cropList = [
-        selection.left, selection.top, selection.width, selection.height];
-    var data = {operations: [['crop', cropList]]};
+    var cropParameters = resize.getSelection();
+    cropParameters.ratio = resize.getRatio();
+
+    var data = {operations: [['crop', cropParameters]]};
 
     var failure = function(xhr) {
         document.querySelector('html').innerHTML = xhr.responseText;
@@ -278,4 +308,4 @@ resize.notifyChange = function() {
     request.open('PATCH', url);
     request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify(data));
-}
+};
